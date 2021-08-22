0c49c0aacf063f09c132600a96a1ee81a075269a
Panagiotis Skarlas <panossk2@gmail.com>
2021-08-22 17:04:53 +0300

    improve production deployment

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,12 +2,13 @@
 #
 # This file should populate the ceryx server with our front-end
 
-set -e
+set -ex
 
 echo "Register the API to Ceryx"
+sleep 5
 curl -H "Content-Type: application/json" \
      -X POST \
-     -d '{"source":"${CERYX_API_HOSTNAME}","target":"${REDIRCT_HOST}:3000"}' \
-     http://${CERYX_API_HOSTNAME}:5555/api/routes/
+     -d '{"source":"'${REDIRCT_DOMAIN:-test.com}'","target":"'${REDIRCT_INTERNAL_HOST:-web}':3000"}' \
+     http://${CERYX_API_HOSTNAME:-api}:5555/api/routes/
 
 exec "$@"
